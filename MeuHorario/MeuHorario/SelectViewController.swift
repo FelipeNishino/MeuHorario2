41529d074e38c69fd2a1404cbb61a02467023a4e
FelipeNishino <49385633+FelipeNishino@users.noreply.github.com>
2021-02-26 15:11:14 -0300

    Cadastro completo

--- MeuHorario/MeuHorario/SelectViewController.swift
+++ MeuHorario/MeuHorario/SelectViewController.swift
@@ -7,31 +7,72 @@
 
 import UIKit
 
-class SelectViewController : UIViewController, UINavigationControllerDelegate {
-    let lbl : UILabel = {
-        let label = UILabel()
-        label.text = "Tela de Seleção"
-        label.textAlignment = .center
-        label.frame = CGRect(x: 0, y: 200, width: 600, height: 40)
-        label.font = .systemFont(ofSize: 22)
-        label.textColor = UIColor(withHex: 0x004687)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+enum ContentType : Int {
+    case courses
+    case periods
+}
+
+class SelectViewController : UIViewController, UITableViewDataSource, UITableViewDelegate, UINavigationControllerDelegate {
+    weak var delegate : RegisterViewController?
+    var senderIndex : Int?
     
+    private let myTableView = UITableView()
+    private let defaultTableCellHeight : CGFloat = 44.0
+    
+    private let dados = [["BCC", "BSI", "TADS"], ["primeiro", "segundo", "terceiro", "quarto", "quinto", "sexto"]]
+    
+//    private let cursos = ["BCC", "BSI", "TADS"]
+//    private let semestres = ["primeiro", "segundo", "terceiro", "quarto", "quinto", "sexto"]
+    private var tableCategory : ContentType?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.title = "Cursos"
+        
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "SelectionTableCell")
+        
+        self.navigationController?.delegate = self
+        
+        self.view.addSubview(myTableView)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableCategory = ContentType(rawValue: senderIndex ?? 0)
+        self.navigationController?.navigationBar.topItem?.title = self.tableCategory == .courses ? "Cursos" : "Semestres"
         self.navigationController?.navigationBar.isHidden = false
-        view.addSubview(lbl)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        myTableView.frame = CGRect(x: self.view.safeAreaInsets.left, y: self.view.safeAreaInsets.top , width: self.view.frame.width - 2 * self.view.safeAreaInsets.right, height: self.view.frame.height)
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        UserDefaults.didAlreadyLaunch = true
+//        UserDefaults.didAlreadyLaunch = true
     }
     
     override func viewDidLayoutSubviews() {
 //        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|v0|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0" : lbl]))
 //        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|v0|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0" : lbl]))
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dados[tableCategory?.rawValue ?? 0].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "SelectionTableCell", for: indexPath)
+        myCell.textLabel!.text = dados[tableCategory?.rawValue ?? 0][indexPath.row]
+        return myCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableCategory == .courses {
+            delegate?.chosenValues[1] = nil
+            delegate?.notifyReload(forCell: 1)
+        }
+        delegate?.chosenValues[tableCategory?.rawValue ?? 0] = tableView.cellForRow(at: indexPath)?.textLabel?.text
+        delegate?.notifyReload(forCell: tableCategory?.rawValue ?? 0)
+        self.navigationController?.popViewController(animated: true)
     }
 }
